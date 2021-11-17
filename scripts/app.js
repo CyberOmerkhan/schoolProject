9cb00bb07e51d8a2890de81a40587b49cf57371f
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-17 16:10:11 +0600

    v1.0

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+let searchBtn = document.querySelector('.bx-search');
+btn.onclick = () => {
+    sidebar.classList.toggle('active');
+}
