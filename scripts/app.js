195ec0e0d463e73220ff25890a12ae132f428003
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-12-12 16:21:24 +0600

    v1.0

--- scripts/app.js
+++ scripts/app.js
@@ -4,5 +4,12 @@ let sidebar = document.getElementsByClassName('sidebar')[0];
 btn.onclick = () => {
     sidebar.classList.toggle('active');
 }
-let logoutButton = document.querySelector('.log_out');
-console.log(logoutButton);
+let logoutButton = $('#log_out');
+let loginButton = $('#log_in');
+if(logedin == false){
+    logoutButton.hide(1);
+    loginButton.show(1);
+}else{
+    loginButton.hide(1);
+    logoutButton.show(1);
+}
